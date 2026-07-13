f11a834cf9a937ca57b816ce9b744e6ee06d131c
Thiago Simiano <simiano9@gmail.com>
2026-07-12 22:34:20 -0300

    config: add supabase url and anon key

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,4 +1,4 @@
 export const SUPABASE_CONFIG = {
-  url: "", // Insira aqui sua URL do Supabase (ex: https://xxxx.supabase.co)
-  anonKey: "" // Insira aqui sua chave anônima (anon/public key) do Supabase
+  url: "https://hvmakyeeclsmkcwqqvwk.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2bWFreWVlY2xzbWtjd3FxdndrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5MDU3MjksImV4cCI6MjA5OTQ4MTcyOX0.mfmCwC4Ed5nC4N-qIZGWmk_9MAwcd_ppJReTRZqLCEw"
 };
